27e3d03c6a2fc46dfbd15707ead4b5bd2ffa9602
Nathan Smith <webalkymst@gmail.com>
2023-11-14 18:33:19 -0700

    update banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -303,7 +303,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            CTO, full-stack engineer, founder, and amateur music producer.
+            CTO, full-stack engineer, founder, and amateur DJ.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Nathaniel Smith, a fullstack software engineer and entrepreneur
